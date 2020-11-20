da684950d0489b6a8cbd827046e65bf45a27ffe1
PinguwuNoot <hanjasn@gmail.com>
2020-11-20 03:25:18 -0800

    refactored WrapLayout class to fit CheckStyle requirements

--- src/main/ui/WrapLayout.java
+++ src/main/ui/WrapLayout.java
@@ -110,38 +110,7 @@ public class WrapLayout extends FlowLayout {
 
             //  Fit components into the allowed width
 
-            Dimension dim = new Dimension(0, 0);
-            int rowWidth = 0;
-            int rowHeight = 0;
-
-            int nmembers = target.getComponentCount();
-
-            for (int i = 0; i < nmembers; i++) {
-                Component m = target.getComponent(i);
-
-                if (m.isVisible()) {
-                    Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
-
-                    //  Can't add the component to current row. Start a new row.
-
-                    if (rowWidth + d.width > maxWidth) {
-                        addRow(dim, rowWidth, rowHeight);
-                        rowWidth = 0;
-                        rowHeight = 0;
-                    }
-
-                    //  Add a horizontal gap for all components after the first
-
-                    if (rowWidth != 0) {
-                        rowWidth += hgap;
-                    }
-
-                    rowWidth += d.width;
-                    rowHeight = Math.max(rowHeight, d.height);
-                }
-            }
-
-            addRow(dim, rowWidth, rowHeight);
+            Dimension dim = layoutSizeSubMethodA(target, preferred, hgap, maxWidth);
 
             dim.width += horizontalInsetsAndGap;
             dim.height += insets.top + insets.bottom + vgap * 2;
@@ -151,13 +120,53 @@ public class WrapLayout extends FlowLayout {
             //  target containter so shrinking the container size works
             //  correctly. Removing the horizontal gap is an easy way to do this.
 
-            Container scrollPane = SwingUtilities.getAncestorOfClass(JScrollPane.class, target);
-
-            if (scrollPane != null && target.isValid()) {
-                dim.width -= (hgap + 1);
-            }
+            layoutSizeSubMethodB(target, hgap, dim);
 
             return dim;
+        }
+    }
+
+    private Dimension layoutSizeSubMethodA(Container target, boolean preferred, int hgap, int maxWidth) {
+        Dimension dim = new Dimension(0, 0);
+        int rowWidth = 0;
+        int rowHeight = 0;
+
+        int nmembers = target.getComponentCount();
+
+        for (int i = 0; i < nmembers; i++) {
+            Component m = target.getComponent(i);
+
+            if (m.isVisible()) {
+                Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
+
+                //  Can't add the component to current row. Start a new row.
+
+                if (rowWidth + d.width > maxWidth) {
+                    addRow(dim, rowWidth, rowHeight);
+                    rowWidth = 0;
+                    rowHeight = 0;
+                }
+
+                //  Add a horizontal gap for all components after the first
+
+                if (rowWidth != 0) {
+                    rowWidth += hgap;
+                }
+
+                rowWidth += d.width;
+                rowHeight = Math.max(rowHeight, d.height);
+            }
+        }
+
+        addRow(dim, rowWidth, rowHeight);
+        return dim;
+    }
+
+    private void layoutSizeSubMethodB(Container target, int hgap, Dimension dim) {
+        Container scrollPane = SwingUtilities.getAncestorOfClass(JScrollPane.class, target);
+
+        if (scrollPane != null && target.isValid()) {
+            dim.width -= (hgap + 1);
         }
     }
 
